a6b9f5ed08e570c49cf087cd13782e2a2778bbf6
Your Name <you@example.com>
2020-01-14 15:53:34 -0500

    add web controls for camera mode

--- controls.py
+++ controls.py
@@ -2,15 +2,16 @@ from network import controller_listener
 
 CAMERA_MODE_RAW = 'R'
 CAMERA_MODE_BALL = 'B'
+CAMERA_MODE_LOADING_BAY = 'L'
 CAMERA_MODE_HEXAGON = 'H'
 
 class Controls():
 
     def __init__(self):
         self.enable_camera = True
-        self.enable_processing = False
-        self.camera_mode = CAMERA_MODE_RAW
-
+        # self.enable_processing = False
+        self.camera_mode = CAMERA_MODE_LOADING_BAY
+        self.enable_streaming = True
         self.enable_feed = True
         self.turn_camera_off = False
 
--- web/www/app.js
+++ web/www/app.js
@@ -6,7 +6,7 @@ new Vue({
   data: {
     controls: {
         enable_camera: false,
-        enable_processing: false,
+        // enable_processing: false,
         camera_mode: 'R'
     }
   },
@@ -16,8 +16,44 @@ new Vue({
   methods: {
     enableCamera: function () {
         var self = this;
-        self.controls.enable_camera = self.controls.enable_camera
+        if(self.controls.enable_camera == false){
+            self.controls.enable_camera = true;
+        }
+        else{
+            self.controls.enable_camera = false;
+        }
+        Socket.send(self.controls)
+    },
+    enableRaw: function() {
+        var self = this;
+        self.controls.camera_mode = 'R';
+        Socket.send(self.controls)
+    },
+    enableBall: function(){
+        var self = this;
+        self.controls.camera_mode = 'B';
+        Socket.send(self.controls)
+    },
+    enableHex: function(){
+        var self = this;
+        self.controls.camera_mode = 'H'
+        Socket.send(self.controls)
+    },
+    enableBay: function(){
+        var self = this;
+        self.controls.camera_mode = 'L'
         Socket.send(self.controls)
     }
+    // enableProcessing: function(){
+    //     var self = this;
+    //     if(self.controls.enable_processing == false){
+    //       self.controls.enable_processing = true;
+    //     }
+    //     else{
+    //       self.controls.enable_processing = false;
+    //     }
+    //     Socket.send(self.controls)
+    // },
+    
   }
 });
